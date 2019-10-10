30fd3607fab60ae289268ceac03081f551dffda4
Celso Crivelaro <celso.crivelaro@revelo.com.br>
2019-10-10 19:00:19 -0300

    Only fuzzy

--- aula9/fuzzy.py
+++ aula9/fuzzy.py
@@ -35,7 +35,6 @@ are defined in scikit-fuzzy as follows
 """
 import numpy as np
 import skfuzzy as fuzz
-import matplotlib.pyplot as plt
 
 # Generate universe variables
 #   * Quality and service on subjective ranges [0, 10]
@@ -55,38 +54,7 @@ tip_lo = fuzz.trimf(x_tip, [0, 0, 13])
 tip_md = fuzz.trimf(x_tip, [0, 13, 25])
 tip_hi = fuzz.trimf(x_tip, [13, 25, 25])
 
-# Visualize these universes and membership functions
-fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(8, 9))
-
-ax0.plot(x_qual, qual_lo, 'b', linewidth=1.5, label='Bad')
-ax0.plot(x_qual, qual_md, 'g', linewidth=1.5, label='Decent')
-ax0.plot(x_qual, qual_hi, 'r', linewidth=1.5, label='Great')
-ax0.set_title('Food quality')
-ax0.legend()
-
-ax1.plot(x_serv, serv_lo, 'b', linewidth=1.5, label='Poor')
-ax1.plot(x_serv, serv_md, 'g', linewidth=1.5, label='Acceptable')
-ax1.plot(x_serv, serv_hi, 'r', linewidth=1.5, label='Amazing')
-ax1.set_title('Service quality')
-ax1.legend()
-
-ax2.plot(x_tip, tip_lo, 'b', linewidth=1.5, label='Low')
-ax2.plot(x_tip, tip_md, 'g', linewidth=1.5, label='Medium')
-ax2.plot(x_tip, tip_hi, 'r', linewidth=1.5, label='High')
-ax2.set_title('Tip amount')
-ax2.legend()
-
-# Turn off top/right axes
-for ax in (ax0, ax1, ax2):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-
-plt.tight_layout()
-
 """
-.. image:: PLOT2RST.current_figure
 
 Fuzzy rules
 -----------
@@ -140,26 +108,6 @@ active_rule3 = np.fmax(qual_level_hi, serv_level_hi)
 tip_activation_hi = np.fmin(active_rule3, tip_hi)
 tip0 = np.zeros_like(x_tip)
 
-# Visualize this
-fig, ax0 = plt.subplots(figsize=(8, 3))
-
-ax0.fill_between(x_tip, tip0, tip_activation_lo, facecolor='b', alpha=0.7)
-ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
-ax0.fill_between(x_tip, tip0, tip_activation_md, facecolor='g', alpha=0.7)
-ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
-ax0.fill_between(x_tip, tip0, tip_activation_hi, facecolor='r', alpha=0.7)
-ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
-ax0.set_title('Output membership activity')
-
-# Turn off top/right axes
-for ax in (ax0,):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-
-plt.tight_layout()
-
 """
 .. image:: PLOT2RST.current_figure
 
@@ -186,29 +134,9 @@ aggregated = np.fmax(tip_activation_lo,
 
 # Calculate defuzzified result
 tip = fuzz.defuzz(x_tip, aggregated, 'centroid')
-tip_activation = fuzz.interp_membership(x_tip, aggregated, tip)  # for plot
-
-# Visualize this
-fig, ax0 = plt.subplots(figsize=(8, 3))
-
-ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
-ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
-ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
-ax0.fill_between(x_tip, tip0, aggregated, facecolor='Orange', alpha=0.7)
-ax0.plot([tip, tip], [0, tip_activation], 'k', linewidth=1.5, alpha=0.9)
-ax0.set_title('Aggregated membership and result (line)')
-
-# Turn off top/right axes
-for ax in (ax0,):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-
-plt.tight_layout()
+print(tip)
 
 """
-.. image:: PLOT2RST.current_figure
 
 Final thoughts
 --------------
